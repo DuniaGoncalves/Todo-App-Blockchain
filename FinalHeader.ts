a1d0cdaf0a07690ee81c3934dee9fcb2350e5ec7
Dunia Goncalves <dgoncalves@everquote.com>
2019-07-05 14:18:29 -0400

    List Tasks

---
+++ FinalHeader.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FinalHeader = styled.h3`
+  font-size: 1.25rem;
+  font-weight: 400;
+  margin: 0;
+  padding: .5rem;
+  text-align: center;
+`;
+
+export default FinalHeader;
