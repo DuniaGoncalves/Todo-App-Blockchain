a1e32d030173828a0184be3dd2dfc8374d1c7a34
Dunia Goncalves <dgoncalves@everquote.com>
2019-07-05 15:07:57 -0400

    Toggle Tasks

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -28,7 +28,16 @@ contract('TodoList', (accounts) => {
     assert.equal(taskCount, 2)
     const event = result.logs[0].args
     assert.equal( event.id.toNumber(), 2)
-    assert.equal(event.content, "A new Task")
+    assert.equal(event.content, 'A new Task')
     assert.equal(event.completed, false)
+  })
+
+  it('toggle task completion', async () => {
+    const result = await this.todoList.toggleCompleted(1)
+    const task = await this.todoList.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal( event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
   })
 })
